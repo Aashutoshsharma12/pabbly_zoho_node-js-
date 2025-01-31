186e081449a1fe5186bb8d2473bd8b7a0f2c810a
Aashutosh sharma <aashutosh.2021@tecorb.co>
2025-01-31 17:39:01 +0530

    commit2

--- server.mjs
+++ server.mjs
@@ -1,6 +1,23 @@
 import express from 'express'
 const app = express();
 app.use(express.json());
+app.get('/list', async (req, res) => {
+    const array = [
+        {
+            _id: 1,
+            name: "Aashutosh"
+        },
+        {
+            _id: 2,
+            name: "Aashutosh sharma"
+        },
+        {
+            _id: 3,
+            name: "Punit Sharma"
+        }
+    ]
+    res.send({ data: array, code: 200 })
+})
 app.listen(4001, () => {
     console.log('Server is running on 4001 port')
 });
